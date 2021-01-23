28b0d6cd720ac668613d7a7796f8baeeafaa6502
Carlos Henrique <carlos.silva@novaandradina.org>
2021-01-23 14:52:30 -0400

    add deploy configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,5 +71,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ["vee-validate/dist/rules"],
-  }
+  },
+
+  //Deploy
+  target: 'static',
+  ssr: false
 }
